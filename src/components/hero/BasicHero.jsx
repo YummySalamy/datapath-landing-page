c2181e43f08e91fa9644f13b98877ffbe56c4364
Sebastian <carsebastian1013@gmail.com>
2024-09-25 16:56:56 -0500

    changed some styles in various components

--- src/components/hero/BasicHero.jsx
+++ src/components/hero/BasicHero.jsx
@@ -6,7 +6,7 @@ const BasicHero = ({ title, description, buttonText, buttonLink }) => {
     return (
         <div className='basic-hero'>
             <div className='basic-hero__content'>
-                <h1 className='basic-hero__highlight'>Inicia tu carrera en Data e Inteligencia Artificial</h1>
+                <h1 className='basic-hero__highlight'>Aprenda habilidades de datos e IA</h1>
                 {/* <span className='basic-hero__highlight'>DataPath</span> */}
                 <p>{description}</p>
                 <div className='basic-hero__buttons'>
